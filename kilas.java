8c712dcd2af6fa202ccf2cb6859a3eae62e37838
krish <alexandergarces987k@gmail.com>
2023-04-17 21:05:37 +0545

    first commit

---
+++ kilas.java
@@ -0,0 +1,26 @@
+public class kilas {
+    public static void main(String[] args) {
+        System.out.println("Hello World"); // the only difference in using println and print is it adds a new line while
+                                           // only print does not add a new line
+        System.out.println("Hello World");
+        System.out.print("Hi");
+        System.out.print("Hi\n");
+
+// about the variables
+int x = 8 ;
+ x = 9 ; // now the value of x is 9 
+String carname = " Lamborghini";
+System.out.println(x);
+System.out.println(carname); // prints Lamborghini
+
+ boolean isfine = true ;
+ 
+ System.out.println("Hari is fine and it's" + isfine);
+
+
+String myname = "krish" ;
+System.out.println(myname.toUpperCase());
+
+
+    }
+}
