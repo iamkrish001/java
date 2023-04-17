4672ee2a6f0d74cf5f5d5a60253a66097c5f9293
krish <alexandergarces987k@gmail.com>
2023-04-17 21:49:19 +0545

    math functions done

--- kilas.java
+++ kilas.java
@@ -17,9 +17,27 @@ public class kilas {
 
         System.out.println("Hari is fine and it's" + isfine);
 
-        String myname = "krish is my name";
+        String myname = "krish is my name"; // string types
         System.out.println(myname.toUpperCase());
         System.out.print(myname.indexOf("name"));
 
+        // to concatenate two or more strings , concatenate means introducing a chain or
+        // simply continuation
+        String first = " Krish";
+        String second = "Bhurtel";
+        System.out.println(first.concat(second));
+        System.out.println(myname.indexOf("i"));
+        System.out.println("My name is\"Krish\"Bhurtel");
+
+        // Java math
+      // Math.max(x,y), prints the maxiumum number
+      int highest =  Math.max(4,6);
+       System.out.println(highest);
+      // MaTH.min(x,y), prints the lowest number
+      int lowest =  Math.min(4,6);
+       System.out.println(lowest);
+       // Math.sqrt , prints the square root number
+       //Math.random, generates random number between 0 - 1
+       System.out.println((int)Math.random()*101); // 1 to 100 , int only let's you to print single integer
     }
 }
