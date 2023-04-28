ec20c911bc4853b0266bb3070fc50052ed5e2fb9
krish <alexandergarces987k@gmail.com>
2023-04-28 09:19:18 +0545

    learned about star pattern and 2d array

--- arrays.java
+++ arrays.java
@@ -15,7 +15,8 @@ public class arrays {
         // }
 
         // // using for-each loop
-        // for (String c : students) // variable c stores the content of array students
+        // for (String c : students) // variable c stores the content of array named
+        // students
         // and prints in the next line
         // {
         // System.out.println(c);
@@ -27,23 +28,37 @@ public class arrays {
         // System.out.println(d); }
 
         String[][] students = { { "Ram", "Sam", " Hari ", "Sita" },
-                                { "pass", " fail", "pass", " fail" }
-                                };
-        // System.out.println(students[1][3]); // the above bracket acts like a matrix , and the answer is printed from 2nd
-                                            // row's 4th column's element
+                { "pass", " fail", "pass", " fail" }
+        };
+        // System.out.println(students[1][3]); // the above bracket acts like a matrix ,
+        // and the answer is printed from 2nd
+        // row's 4th column's element
 
-//  for(String s :students[1]) // gives 2nd row's values to variable 's'
-//  {System.out.println( s);}
+        // for(String s :students[1]) // gives 2nd row's values to variable 's'
+        // {System.out.println( s);}
 
+        // another example of multi dimensional array
+        // int num[][] = { { 1, 2, 3 },
+        //                 { 4, 5, 6 } }; // variable declaration of 2 set of numbers
 
-int i, j  ;
+        // System.out.println(num[0][2]); // prints 3
 
- for(i=0;i< students.length; i++)// i denotes the row and j denotes the column
- {
-for(j=0; j<students[i].length;j++){
-System.out.println(students[i][j]);
+// WAP to print decrement star pattern starting from 5 stars
+for(int i=1; i<=5; i++){
+for(int j=5;j>=i;j--)
+{
+    System.out.println("*");
 }
- }
+System.out.println("");
+}
+
+for(int i=1;i<=5;i++)
+{
+    for(int j=1;j<=i;j++)
+    {
+        System.out.println("*");
+    }
+}
 
     }
 }
