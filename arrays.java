62c6196272442a2c89f6d7893edb55d3860ec993
krish <alexandergarces987k@gmail.com>
2023-05-02 20:37:41 +0545

    star pattern 3

--- arrays.java
+++ arrays.java
@@ -51,18 +51,20 @@ public class arrays {
         // }
         // System.out.println("");
         // }
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j <i; j++) {
-                System.out.print("*");
-            }
-            System.out.println("");// gives a new line for spacing
-        }
-        for (int i = 0; i < 5; i++) {
-            for (int j =5; j > i; j--) {
-                System.out.print("*");
-            }
-            System.out.println("");
-        }
-        
+        // for (int i = 0; i < 5; i++) {
+        //     for (int j = 0; j <i; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println("");// gives a new line for spacing
+        // }
+      for(int i=1;i<=5;i++){
+for(int j=i;j<=i;j++){
+    System.out.print("");
+}
+
+for(int j=1;j<=i;j++){
+    System.out.print("*");
+}
+System.out.println(""); }
     }
 }
